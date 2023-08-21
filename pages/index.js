f7f7b62a6754b08c15858adfcd4683462022c0ae
Willi0te <william.sinclair92@gmail.comm>
2023-08-21 19:47:25 +0200

    added new styling and functions to admin panel

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
+import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react";
+import styles from "@/styles/index.module.css";
 
 export default function Home() {
   const { data: session } = useSession();
   if (!session) {
     return (
-      <div className="main">
-        <div className="flex-justify-center">
-          <button onClick={() => signIn("google")} className="sign-in">
+      <div className={styles.main}>
+        <div className={styles.flexJustifyCenter}>
+          <button onClick={() => signIn("google")} className="signIn">
             Login with Google
           </button>
         </div>
       </div>
     );
   }
-  return <div>Logged in as {session.user.email}</div>;
+  return (
+    <div className={styles.loginContainer}>
+      <Nav />
+      <div className={styles.pageContent}>
+        Logged in as {session.user.email}
+      </div>
+    </div>
+  );
 }
