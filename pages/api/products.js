c17209cbb8ad1522c76f46ae9ae763f9c0d7f56c
Willi0te <william.sinclair92@gmail.comm>
2023-08-23 17:26:08 +0200

    fixed POST issue

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,9 +1,7 @@
 // // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import clientPromise from "@/lib/mongodb";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
-import mongoose from "mongoose";
 
 // export default function handler(req, res) {
 //     res.status(200).json({ name: 'John Doe' })
@@ -12,7 +10,6 @@ import mongoose from "mongoose";
 async function handler(req, res) {
   const { method } = req;
   await mongooseConnect();
-  mongoose.Promise = clientPromise;
   if (method === "POST") {
     const { title, description, price } = req.body;
     const productDoc = await Product.create({ title, description, price });
