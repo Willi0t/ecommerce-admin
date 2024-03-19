0982afa1bcd9349b9bb4e59e2f9c3773d7015f5e
Willi0te <william.sinclair92@gmail.com>
2024-03-19 16:34:52 +0100

    adding orders page

--- pages/orders.js
+++ pages/orders.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/orders.module.css";
 import Layout from "@/components/layout";
+import axios from "axios";
 
-function Orders() {
-  return <Layout>Orders page here</Layout>;
+function OrdersPage() {
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        axios.get("/api/orders").then((response) => {
+            setOrders(response.data);
+        });
+    }, []);
+
+    console.log(orders);
+
+    return (
+        <Layout>
+            <h1>Orders</h1>
+            <table className="tableGlobal">
+                <thead>
+                    <tr>
+                        <th>Created AT</th>
+                        <th>Recipient</th>
+                        <th>Products</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.length > 0 &&
+                        orders.map((order) => (
+                            <tr>
+                                <td>{order.createdAt}</td>
+                                <td>
+                                    {order.name} {order.email} <br />
+                                    {order.city} {order.postCode}{" "}
+                                    {order.country} <br />
+                                    {order.streetAddress}
+                                </td>
+                                <td>
+                                    {order.line_items &&
+                                        order.line_items.map((items) => (
+                                            <>
+                                                {
+                                                    items.price_data
+                                                        ?.product_data.name
+                                                }
+                                                {items.quantity}
+                                                <br />
+
+                                                <br />
+                                            </>
+                                        ))}
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </Layout>
+    );
 }
 
-export default Orders;
+export default OrdersPage;
